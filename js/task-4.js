97e8667948be1d96617e1ea6bbfce0123baf6847
Olha Shamrai <olga.moryacka91@gmail.com>
2025-07-06 16:03:18 +0300

    add corrections

--- js/task-4.js
+++ js/task-4.js
@@ -5,7 +5,7 @@
 // Функція має використовувати ланцюжок виклику методів та повертати загальний баланс користувачів(властивість balance),
 // стать яких(властивість gender) збігається зі значенням параметра gender.
 
-function getTotalBalanceByGender(users, gender) {
+const getTotalBalanceByGender = (users, gender) => {
     return users.filter(user => user.gender === gender).reduce((total, user) => total + user.balance, 0);
 }
 
